ada757f8555d284d8469a9b153d10ecedceadc55
roosiu <roosiu@gmail.com>
2024-01-08 21:41:02 +0100

    js update - cursor add

--- script.js
+++ script.js
@@ -6,26 +6,33 @@ window.onload = () => {
 const typeWriter = () => {
     const collection = document.getElementsByClassName("typeWriter");
 
-
   for (let index = 0; index < collection.length; index++) {
     const element = collection[index];
     const dataText = element.dataset.text;
     const array = JSON.parse("[" + dataText + "]");
     typer(element, array);
+    createCursor(element);
   }
 }
 
 
 const typer = (el, text) => {
-
     for (let index = 0; index < text.length; index++) {
         let oneText = text[index];
-
         for (let i = 0; i < oneText.length; i++) {
             let oneLetter = oneText[i];
+            setTimeout(() => {el.innerHTML += oneLetter} , 1000);
             console.log(oneLetter);
         }
     }
-    el.innerHTML = text[0]+"<span class='typewriter-cursor'>|</span>";
 }
 
+
+const createCursor = (el) => {
+    const newSpan = document.createElement("span");
+    const newContent = document.createTextNode("|");
+    newSpan.appendChild(newContent);
+    newSpan.setAttribute("class", "typewriter-cursor");
+    const parentDiv = el.parentElement;
+    parentDiv.appendChild(newSpan);
+}
